493b65504b4015b7916f1782338883c641211062
Kennedy Wambua <kennedy.wambua@alternateltd.com>
2025-04-17 17:07:38 +0300

    Add User-Organization relationship and update schema naming conventions

---
+++ packages/backend/prisma/migrations/20250417140724_added_user_organization_relationship/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Organization` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Organization` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[organization_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `updated_at` to the `Organization` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `full_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `organization_id` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Organization" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "name",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "full_name" TEXT NOT NULL,
+ADD COLUMN     "organization_id" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_organization_id_key" ON "User"("organization_id");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
